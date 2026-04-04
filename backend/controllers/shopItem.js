8c17235ec79cbd2249a6479ccfd53303eff87adc
Suraj Gupta <surajkumar02oct2003@gmail.com>
2026-04-04 15:16:19 +0530

    final correct model name

--- backend/controllers/shopItem.js
+++ backend/controllers/shopItem.js
@@ -1,6 +1,6 @@
 const uploadOnCloudinary = require("../utils/cloudinary")
 const Shop = require("../models/shopModel")
-const ShopItem = require("../models/temp")
+const ShopItem = require("../models/shopItemModel")
 
 
 //add item
